b9a20eb6f9b36401ccc495841fe6cf60702520e2
Ian <holmesian17@gmail.com>
2017-07-18 10:47:47 -0500

    Update tread.py

--- tread.py
+++ tread.py
@@ -2,7 +2,7 @@ import praw
 
 reddit = praw.Reddit(client_id = 'oR2piLKI4FtbNw',
                      client_secret = 'uavxhezWwAFBxRIfC5vYA7eXx-0',
-                     user_agent = 'email-posts by u/spinman17',
+                     user_agent = 'tread by u/spinman17',
                      username = 'username',
                      password = 'password')
 
