73d34c1e29df5f82b1cb5b2eebd8b512242588c5
Ian <holmesian17@gmail.com>
2017-07-18 10:46:40 -0500

    Create tread.py

---
+++ tread.py
@@ -0,0 +1,16 @@
+import praw
+
+reddit = praw.Reddit(client_id = 'oR2piLKI4FtbNw',
+                     client_secret = 'uavxhezWwAFBxRIfC5vYA7eXx-0',
+                     user_agent = 'email-posts by u/spinman17',
+                     username = 'spinman17',
+                     password = 'ich17rdt')
+
+print(reddit.user.me())
+srs = ['baseball', 'hiphopheads', 'python' ]
+
+for sr in srs:
+    subreddit = reddit.subreddit(sr)
+    for submission in subreddit.top(limit=5):
+        print(submission.title)
+        print(submission.url)   
