6833a2da8a75e18252a4e8ed8f52c5ed621b2dcf
Ian <holmesian17@gmail.com>
2017-07-18 12:13:34 -0500

    Update tread.py

--- tread.py
+++ tread.py
@@ -6,19 +6,24 @@ reddit = praw.Reddit(client_id = 'oR2piLKI4FtbNw',
                      client_secret = 'uavxhezWwAFBxRIfC5vYA7eXx-0',
                      user_agent = 'tread by u/spinman17',
                      username = 'username',
-                     password = 'ipassword')
+                     password = 'password')
 
 subscriptions = list(reddit.user.subreddits(limit=None))
-
+email = []
 
 for subscribed in subscriptions:
     pull_list = re.compile(r'Subreddit(display_name=)')
     subscribed = pull_list.sub('', str(subscribed))
     subreddit = reddit.subreddit(subscribed)
-    for submission in subreddit.top(limit=5):
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.login('daily.reddit.updates@gmail.com', 'zxcvbnm1029384756')
-        server.sendmail('daily.reddit.updates.com', 'email address', 'Subject: Here are your reddit threads\n' '\n' + submission.title + submission.url)
-        server.quit()
+    for submission in subreddit.top(limit=1):
+        email.append(submission.title) #TODO: needs to send the title, url of the post, subreddit name and format them nicely
+        email.append(submission.url)
+
+email = ' '.join(email) 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login('daily.reddit.updates@gmail.com', 'zxcvbnm1029384756')
+server.sendmail('daily.reddit.updates@gmail.com', 'holmesian17@gmail.com', 'Subject: Here are your reddit threads\n' '\n' + str(email))
+
+
